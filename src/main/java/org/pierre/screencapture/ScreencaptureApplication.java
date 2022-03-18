08e6f31ac40630fd071ad2d086ca46d33cfdb149
vernetto <Ernesto0z>
2022-03-19 00:59:58 +0100

    first commit, recording mic

---
+++ src/main/java/org/pierre/screencapture/ScreencaptureApplication.java
@@ -0,0 +1,13 @@
+package org.pierre.screencapture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreencaptureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreencaptureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pierre/screencapture/ScreencaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pierre.screencapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreencaptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
